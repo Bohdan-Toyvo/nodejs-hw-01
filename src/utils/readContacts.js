163fe73eb2af8a95870afc4e08a4f8ad988ffbb4
Bohdan <toyvo7@gmail.com>
2025-06-29 15:09:53 +0200

    add utils

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+
+    console.log('Вміст файлу:', data);
+
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (contacts) => {
+  const data = JSON.stringify(contacts, null, 2);
+
+  try {
+    await fs.writeFile(PATH_DB, data, 'utf8');
+  } catch (error) {
+    console.error(error);
+  }
+};
